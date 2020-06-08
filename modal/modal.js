b82f1956e90b2a99637f3ce4bdf6633dbfef51f2
LEON-coder <54926533+LEON-coder@users.noreply.github.com>
2020-06-08 21:03:28 +0300

    Add files via upload

---
+++ modal/modal.js
@@ -0,0 +1,12 @@
+var modalContent = document.getElementsByClassName('modal-content');
+var btn = document.getElementById('submit');
+var home = document.getElementsByClassName('home');
+
+
+function modalContent(event){
+    modalContent.style.display = "flex";
+};
+
+function btn(){
+    modalContent.style.display = "none";
+};
